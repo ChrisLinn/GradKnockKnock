4760b2e99821b5962a33ec40bf2a9281a5598277
ChrisLinn <nanyishanshaoxiu@gmail.com>
2017-08-29 20:04:50 +1000

    IP updated at Tue Aug 29 2017 20:04:50 GMT+1000 (AEST)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,31 +4,8 @@ import './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>React Starter Kit</h1>
-    <h2><a href='https://bodyno.com' target='_blank'>bodyno</a></h2>
-    <IndexLink to='/' activeClassName='route--active'>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='route--active'>
-      Counter
-    </Link>
-    {' · '}
-    <Link to='/zen' activeClassName='route--active'>
-      Zen
-    </Link>
-    {' · '}
-    <Link to='/elapse' activeClassName='route--active'>
-      Elapse
-    </Link>
-    {' · '}
-    <Link to='/route/88' activeClassName='route--active'>
-      Route
-    </Link>
-    {' · '}
-    <Link to='/notFound' activeClassName='route--active'>
-      404
-    </Link>
+    <h1>GradKnockKnock</h1>
+    邮件订阅 UniMelb Careers Online IT 相关 Graduate Program
   </div>
 )
 
--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
 
 export const HomeView = () => (
   <div>
-    <h4>This is a duck, because Redusssx!</h4>
-    <img className='duck' src={DuckImage} />
   </div>
 )
 
